0866fc8dfd00b79c57a066fe6a2e233d6e80987f
Steve <stryan@saintnet.tech>
2020-04-01 13:23:29 -0400

    send even if no message text

--- mautrix_facebook/portal.py
+++ mautrix_facebook/portal.py
@@ -598,6 +598,11 @@ class Portal:
                 content.format = Format.HTML
                 content.formatted_body = f"<a href='{attachment.original_url}'>{attachment.title}</a>"
                 event_id = await intent.send_message(self.mxid, content)
+            else if message_text is None:
+                content = TextMessageEventContent(msgtype=MessageType.TEXT, body=f"{attachment.title}: {attachment.original_url}")
+                content.format = Format.HTML
+                content.formatted_body = f"<a href='{attachment.original_url}'>{attachment.title}</a>"
+                event_id = await intent.send_message(self.mxid, content)
             else:
                 return None
         else:
